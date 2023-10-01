51568f2058bdbc6887130bf09e6c7364b32eb2b3
zzw <3279527604@qq.com>
2023-10-01 20:58:05 +0800

    初始化项目

---
+++ pages/record/record.js
@@ -0,0 +1,24 @@
+Page({
+    data:{
+      ticketRecords: [
+        { start: '起点1', end: '终点1', time: '2小时', cost: '100元' },
+        { start: '起点2', end: '终点2', time: '1.5小时', cost: '80元' },
+        { start: '起点3', end: '终点3', time: '3小时', cost: '120元' },
+      ]
+    },
+    onLoad:function(options){
+      // 页面初始化 options为页面跳转所带来的参数
+    },
+    onReady:function(){
+      // 页面渲染完成
+    },
+    onShow:function(){
+      // 页面显示
+    },
+    onHide:function(){
+      // 页面隐藏
+    },
+    onUnload:function(){
+      // 页面关闭
+    }
+  })
