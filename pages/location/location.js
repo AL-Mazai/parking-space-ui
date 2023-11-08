389fe0171964aa576e31b9c9c76d3c08708de7b7
zzw <3279527604@qq.com>
2023-11-08 17:32:26 +0800

    引入地图

---
+++ pages/location/location.js
@@ -0,0 +1,74 @@
+// pages/location.js
+// 引入SDK核心类
+var QQMapWX = require('../../libs/qqmap-wx-jssdk');
+var qqmapsdk;
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 实例化API核心类
+        qqmapsdk = new QQMapWX({
+            key: 'AU7BZ-7233J-7CWFL-XNDFH-BAHPE-J3BGV'
+        });
+        this.mapCtx = wx.createMapContext('map')
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
